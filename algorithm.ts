9797ee6e19678c9359519f00461528badbbe3b06
HizemMontassar <montassar.hizem@esprit.tn>
2023-01-20 13:55:28 +0100

    fix: updating multiple results case

--- algorithm.ts
+++ algorithm.ts
@@ -1,22 +1,27 @@
-function longestSequence(array: number[]): number {
-    let index = 0
-    let counter = 0
-    let longestSequence = 0
-
-    for(let i=0; i<array.length; i++){
-        if(array[i] === 1){
-            counter ++;
-        }
-        else{
-            if(counter > longestSequence){
-                longestSequence = counter
-                index = i
-            }
-            counter = 0
-        }
+const longestSequence = (array: number[]) => {
+  let oneSunTrack = 0;
+  let zeroSumTrack = 0;
+  let prevLongestSequence = 0;
+  let index = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 1) {
+      oneSunTrack++;
+      if (prevLongestSequence <= oneSunTrack) {
+        index = i - oneSunTrack;
+      }
+    } else {
+      zeroSumTrack++;
+      if (oneSunTrack > prevLongestSequence) {
+        prevLongestSequence = oneSunTrack;
+        index = i;
+      } else if (zeroSumTrack === array.length) {
+        index = array.length - 1;
+      }
+      oneSunTrack = 0;
     }
-    return index;
-}
+  }
+  return index;
+};
 
-const array: number[] = process.argv.slice(3).map(Number);
+const array: number[] = process.argv.slice(2).map(Number);
 console.log(longestSequence(array));
